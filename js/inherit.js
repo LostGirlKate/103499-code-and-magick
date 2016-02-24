97d19b8ed8580abddacf64649daad85922db9804
Ekaterina Kryukova <ekaterina_kryukova90@mail.ru>
2016-02-24 20:44:23 +0300

    module8-task1

---
+++ js/inherit.js
@@ -0,0 +1,12 @@
+'use strict';
+(function() {
+
+  function inherit(child, parent) {
+    function EmptyCtor() {}
+    EmptyCtor.prototype = parent.prototype;
+    child.prototype = new EmptyCtor();
+  }
+
+  window.inherit = inherit();
+
+})();
